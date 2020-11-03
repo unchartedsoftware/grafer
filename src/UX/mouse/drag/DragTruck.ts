18267e245df06db11aee0395780c10b25cf7aa88
Dario Segura <dsegura@uncharted.software>
2020-11-03 14:54:36 -0500

    [UX] Truck accounts for camera rotation

--- src/UX/mouse/drag/DragTruck.ts
+++ src/UX/mouse/drag/DragTruck.ts
@@ -1,17 +1,21 @@
 import {DragBase} from './DragBase';
 import {MouseState} from '../MouseHandler';
-import {vec2, vec3} from 'gl-matrix';
+import {quat, vec2, vec3} from 'gl-matrix';
 
 export class DragTruck extends DragBase {
     protected handleMouse(event: symbol, state: MouseState, delta: vec2): void {
         if (state.buttons[this.button]) {
             const position = this.viewport.camera.position;
-            const distance = vec3.length(position);
-            const vertical = this.viewport.camera.aovRad * distance; // Math.sin(this.viewport.camera.aovRad) * distance;
-            const mult = vertical / this.viewport.rect.height;
+            const rotated = vec3.transformQuat(vec3.create(), position, this.viewport.camera.rotation);
+            const distance = Math.abs(rotated[2]); // vec3.length(position); // use the rotated z distance instead
+            const vertical = this.viewport.camera.aovRad * distance; // good enough approximation
+            const pixelToWorld = vertical / this.viewport.rect.height;
 
-            position[0] += delta[0] * mult;
-            position[1] -= delta[1] * mult;
+            const delta3 = vec3.fromValues(delta[0] * pixelToWorld, delta[1] * -pixelToWorld, 0);
+            const inverse = quat.invert(quat.create(), this.viewport.camera.rotation);
+            vec3.transformQuat(delta3, delta3, inverse);
+
+            vec3.add(position, position, delta3);
             this.viewport.camera.position = position;
 
             this.viewport.render();
