69ce0a2ab156403ea1ba137cb55dcbfa48532d84
Dario Segura <dsegura@uncharted.software>
2020-11-11 16:50:04 -0500

    [GRAPH] Cleaned up circle fragment shader code

--- src/graph/nodes/circle/Circle.fs.glsl
+++ src/graph/nodes/circle/Circle.fs.glsl
@@ -9,31 +9,15 @@ in vec2 vPixelLocation;
 
 out vec4 fragColor;
 
-#define COMPUTE_ANTIALIAS 0 // proper antialias needs two passes
-#if (COMPUTE_ANTIALIAS == 1)
-    #define ANTIALIAS_WIDTH 4.0
-#else
-    #define ANTIALIAS_WIDTH 0.0
-#endif
-
 void main() {
-    float antialiasWidth = min(floor(fPixelRadius * 0.5), ANTIALIAS_WIDTH);
-    float solidRadius = fPixelRadius - antialiasWidth;
+    float solidRadius = max(2.0, fPixelRadius);
     float fromCenter = length(vPixelLocation);
 
     float outlineRadius = fPixelRadius - min(8.0 * uPixelRatio, floor(fPixelRadius * 0.4));
     vec3 color = fColor.rgb * (1.0 - 0.2 * clamp(fromCenter - outlineRadius, 0.0, 1.0));
 
-   if (COMPUTE_ANTIALIAS == 1) {
-       if (fromCenter - solidRadius > antialiasWidth) {
-           discard;
-       }
-       float alpha = 1.0 - max(0.0, fromCenter - solidRadius) / antialiasWidth;
-       fragColor = vec4(color, alpha);
-   } else {
-       if (fromCenter > solidRadius) {
-           discard;
-       }
-       fragColor = vec4(color, 1.0);
+   if (fromCenter > solidRadius) {
+       discard;
    }
+   fragColor = vec4(color, 1.0);
 }
