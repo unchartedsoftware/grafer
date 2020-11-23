4f13a50da762e307c25842f6dba673f3df1add46
Dario Segura <dsegura@uncharted.software>
2020-11-23 17:50:29 -0500

    [GRAPH] Nodes and edges export a `types` objects with constructors

--- src/graph/edges/mod.ts
+++ src/graph/edges/mod.ts
@@ -1,4 +1,12 @@
 export * from './straight/Straight';
 export * from './gravity/Gravity';
 
+import {Straight} from './straight/Straight';
+import {Gravity} from './gravity/Gravity';
+
+export const types = {
+    Straight,
+    Gravity,
+}
+
 export* from './Edges';
--- src/graph/nodes/mod.ts
+++ src/graph/nodes/mod.ts
@@ -1,4 +1,14 @@
 export * from './circle/Circle';
 export * from './ring/Ring';
 
+import {Circle} from './circle/Circle';
+import {Ring} from './ring/Ring';
+
+const types = {
+    Circle,
+    Ring,
+}
+
+export {types};
+
 export * from './Nodes';
