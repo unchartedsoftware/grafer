f0f997f4c178a02bc0ba4cf4d09691741c142405
Dario Segura <dsegura@uncharted.software>
2020-11-23 17:54:25 -0500

    [PROJECT] Module exports grafer utility objects

--- src/mod.ts
+++ src/mod.ts
@@ -3,4 +3,6 @@ export * as graph from './graph/mod';
 export * as loaders from './loaders/mod';
 export * as UX from './UX/mod';
 
+export * from './grafer/mod';
+
 export const message = 'Howdy Worlds!';
