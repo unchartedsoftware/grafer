d1062c18b333ae12e42b12d596f6f4959d458b46
Dario Segura <dsegura@uncharted.software>
2020-11-24 17:07:10 -0500

    [EXAMPLES] Cleaned up basic example

--- examples/src/simple/basic.ts
+++ examples/src/simple/basic.ts
@@ -4,24 +4,14 @@ import '../../../src/grafer/GraferView';
 export async function basic(container: HTMLElement): Promise<void> {
     const nodes = {
         data: [
-                { x: -10.0, y: 10.0, radius: 1.0 },
-                { x: 10.0, y: 10.0, radius: 1.2 },
-                { x: 0.0, y: -10.0, radius: 1.4 },
-                { x: 0.0, y: 0.0, radius: 1.6 },
-        ],
-    };
-
-    const ringNodes = {
-        type: 'Ring',
-        data: [
-            { x: -20.0, y: -20.0, radius: 1.8 },
-            { x: 20.0, y: -20.0, radius: 2.0 },
-            { x: 0.0, y: 20.0, radius: 2.2 },
+                { x: -8.6, y: 5.0 },
+                { x: 8.6, y: 5.0 },
+                { x: 0.0, y: -10.0 },
+                { x: 0.0, y: 0.0 },
         ],
     };
 
     const edges = {
-        type: 'Gravity',
         data: [
             { source: 0, target: 1 },
             { source: 1, target: 2 },
@@ -33,18 +23,9 @@ export async function basic(container: HTMLElement): Promise<void> {
         ],
     };
 
-    const ringEdges = {
-        data: [
-            { source: 0, target: 1 },
-            { source: 1, target: 2 },
-            { source: 2, target: 0 },
-        ],
-    };
-
     const layers = [
         { nodes, edges },
-        { nodes: ringNodes, edges: ringEdges },
     ];
 
-    render(html`<grafer-view class="grafer_container" .layers="${layers}"></grafer-view>`, container);
+    render(html`<grafer-view class="grafer_container" .layers="${layers}"></grafer-view><mouse-interactions></mouse-interactions>`, container);
 }
