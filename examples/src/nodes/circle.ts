56910b7ff0aaf33ae849d1a06e01f47a1e4695cc
Dario Segura <dsegura@uncharted.software>
2020-12-01 15:30:18 -0500

    [EXAMPLES] Added basic node shapes examples

---
+++ examples/src/nodes/circle.ts
@@ -0,0 +1,36 @@
+import {html, render} from 'lit-html';
+import '../../../src/grafer/GraferView';
+
+function createNodePoints(count: number, radius: number = 10.0): any[] {
+    const PI2 = Math.PI * 2;
+    const degStep = (PI2) / count;
+    const result = [];
+
+    for (let angle = 0, i = 0; angle < PI2; angle += degStep, ++i) {
+        const pX = Math.cos(angle) * radius;
+        const pY = Math.sin(angle) * radius;
+        result.push({
+            id: `p${i}-${radius}`,
+            x: pX,
+            y: pY,
+        });
+    }
+
+    return result;
+}
+
+export async function circle(container: HTMLElement): Promise<void> {
+    const nodes = {
+        type: 'Circle',
+        data: [
+            ...createNodePoints(10, 10),
+            ...createNodePoints(5, 5),
+        ],
+    };
+
+    const layers = [
+        { nodes },
+    ];
+
+    render(html`<grafer-view class="grafer_container" .layers="${layers}"></grafer-view><mouse-interactions></mouse-interactions>`, container);
+}
---
+++ examples/src/nodes/mod.ts
@@ -0,0 +1,3 @@
+export * from './circle';
+export * from './ring';
+export * from './triangle';
