e32695719de83f01eefdea8eae83f014141b7c2b
Dario Segura <dsegura@uncharted.software>
2020-12-01 15:23:39 -0500

    [GRAPH] Added triangle node shape

--- src/graph/nodes/shaders/shapes.glsl
+++ src/graph/nodes/shaders/shapes.glsl
@@ -7,8 +7,19 @@ float opOnion(in float d, in float r) {
     return abs(d) - r;
 }
 
-float sdCircle(vec2 p, float r ) {
+float sdCircle(in vec2 p, in float r ) {
     return length(p) - r;
+}
+
+float sdEquilateralTriangle(in vec2 p, in float r) {
+    const float k = sqrt(3.0);
+    p.x = abs(p.x) - r;
+    p.y = p.y + (r + 0.5)/k;
+    if (p.x + k * p.y > 0.0) {
+        p = vec2(p.x-k*p.y,-k*p.x-p.y) / 2.0;
+    }
+    p.x -= clamp(p.x, -2.0 * r, 0.0);
+    return -length(p) * sign(p.y);
 }
 
 
