213cf5ca2c733de70c2ad4103a73e717cfb6e0f6
Dario Segura <dsegura@uncharted.software>
2020-12-01 15:23:54 -0500

    [GRAPH] Added missing export

--- src/graph/nodes/mod.ts
+++ src/graph/nodes/mod.ts
@@ -1,12 +1,15 @@
 export * from './circle/Circle';
 export * from './ring/Ring';
+export * from './triangle/Triangle';
 
 import {Circle} from './circle/Circle';
 import {Ring} from './ring/Ring';
+import {Triangle} from './triangle/Triangle';
 
 const types = {
     Circle,
     Ring,
+    Triangle,
 };
 
 export {types};
