19355f6473c71cf80ff83a2e14dc605ada34ff4d
Dario Segura <dsegura@uncharted.software>
2020-12-01 15:48:29 -0500

    [GRAPH] Added pentagon node shape

--- src/graph/nodes/mod.ts
+++ src/graph/nodes/mod.ts
@@ -1,15 +1,18 @@
 export * from './circle/Circle';
 export * from './ring/Ring';
 export * from './triangle/Triangle';
+export * from './pentagon/Pentagon';
 
 import {Circle} from './circle/Circle';
 import {Ring} from './ring/Ring';
 import {Triangle} from './triangle/Triangle';
+import {Pentagon} from './pentagon/Pentagon';
 
 const types = {
     Circle,
     Ring,
     Triangle,
+    Pentagon,
 };
 
 export {types};
--- src/graph/nodes/shaders/shapes.glsl
+++ src/graph/nodes/shaders/shapes.glsl
@@ -22,4 +22,14 @@ float sdEquilateralTriangle(in vec2 p, in float r) {
     return -length(p) * sign(p.y);
 }
 
+float sdPentagon(in vec2 p, in float r) {
+    const vec3 k = vec3(0.809016994, 0.587785252, 0.726542528);
+    p.y = -(p.y + 0.1) * 1.175;
+    p.x = abs(p.x) * 1.175;
+    p -= 2.0 * min(dot(vec2(-k.x, k.y), p), 0.0) * vec2(-k.x, k.y);
+    p -= 2.0 * min(dot(vec2(k.x, k.y), p), 0.0) * vec2(k.x, k.y);
+    p -= vec2(clamp(p.x, -r*k.z, r*k.z), r);
+    return length(p) * sign(p.y);
+}
+
 
