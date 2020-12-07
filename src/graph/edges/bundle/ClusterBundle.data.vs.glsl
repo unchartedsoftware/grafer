c564bef1b9887d2f29be1fe8308e0afb9dc6c2b1
Dario Segura <dsegura@uncharted.software>
2020-12-07 15:16:27 -0500

    [GRAPH] Initial cluster bundle edge type implementation

---
+++ src/graph/edges/bundle/ClusterBundle.data.vs.glsl
@@ -0,0 +1,88 @@
+#version 300 es
+
+layout(location=0) in uint aSourceIndex;
+layout(location=1) in uint aTargetIndex;
+layout(location=2) in uint aSourceClusterIndex;
+layout(location=3) in uint aTargetClusterIndex;
+layout(location=4) in uint aSourceColor;
+layout(location=5) in uint aTargetColor;
+layout(location=6) in uint aIndex;
+
+uniform sampler2D uGraphPoints;
+
+out vec3 vSource;
+out vec3 vTarget;
+out vec3 vControl;
+flat out uint vSourceColor;
+flat out uint vTargetColor;
+out vec2 vColorMix;
+
+#pragma glslify: valueForIndex = require(../../../renderer/shaders/valueForIndex.glsl)
+
+void main() {
+    vec4 source = valueForIndex(uGraphPoints, int(aSourceIndex));
+    vec4 target = valueForIndex(uGraphPoints, int(aTargetIndex));
+    vec4 sourceCluster = valueForIndex(uGraphPoints, int(aSourceClusterIndex));
+    vec4 targetCluster = valueForIndex(uGraphPoints, int(aTargetClusterIndex));
+
+    vec3 direction = normalize(vec3(targetCluster.xy, 0.0) - vec3(sourceCluster.xy, 0.0));
+    vec3 sourceClusterEdge = sourceCluster.xyz + direction * sourceCluster[3];
+    vec3 targetClusterEdge = targetCluster.xyz - direction * targetCluster[3];
+
+    float edgeToEdge = length(targetClusterEdge - sourceClusterEdge);
+    vec3 bundlePoint = sourceClusterEdge + direction * (edgeToEdge * 0.5);
+
+    vec3 sourceEdgeToNode = sourceClusterEdge - source.xyz - direction * source[3];
+    float sourceNodeAdjacent = dot(normalize(sourceEdgeToNode), direction) * length(sourceEdgeToNode);
+    vec3 sourceClusterControl = sourceClusterEdge - direction * min(sourceNodeAdjacent * 0.75, sourceCluster[3]);
+    vec3 sourceControlDirection = normalize(sourceClusterControl - source.xyz);
+    vec3 sourcePoint = source.xyz + sourceControlDirection * source[3];
+
+    vec3 targetEdgeToNode = target.xyz - targetClusterEdge - direction * target[3];
+    float targetNodeAdjacent = dot(normalize(targetEdgeToNode), direction) * length(targetEdgeToNode);
+    vec3 targetClusterControl = targetClusterEdge + direction * min(targetNodeAdjacent * 0.75, targetCluster[3]);
+    vec3 targetControlDirection = normalize(targetClusterControl - target.xyz);
+    vec3 targetPoint = target.xyz + targetControlDirection * target[3];
+
+    // TODO: Optimize this to avoid branches. (If performance becomes a problem)
+    if (aIndex == 0u) {
+        if (aSourceIndex == aSourceClusterIndex) {
+            vSource = sourcePoint;
+            vControl = sourcePoint;
+            vTarget = sourcePoint;
+        } else {
+            vSource = sourcePoint;
+            vControl = sourceClusterControl;
+            vTarget = (sourceClusterControl + bundlePoint) / 2.0;
+        }
+    } else if (aIndex == 1u) {
+        if (aSourceIndex == aSourceClusterIndex) {
+            vSource = sourcePoint;
+        } else {
+            vSource = (sourceClusterControl + bundlePoint) / 2.0;
+        }
+
+        vControl = bundlePoint;
+
+        if (aTargetIndex == aTargetClusterIndex) {
+            vTarget = targetPoint;
+        } else {
+            vTarget = (bundlePoint + targetClusterControl) / 2.0;
+        }
+    } else {
+        if (aTargetIndex == aTargetClusterIndex) {
+            vSource = targetPoint;
+            vControl = targetPoint;
+            vTarget = targetPoint;
+        } else {
+            vSource = (bundlePoint + targetClusterControl) / 2.0;
+            vControl = targetClusterControl;
+            vTarget = targetPoint;
+        }
+    }
+
+    vSourceColor = aSourceColor;
+    vTargetColor = aTargetColor;
+
+    vColorMix = vec2(float(aIndex) * 0.25, float(aIndex + 1u) * 0.25);
+}
---
+++ src/graph/edges/bundle/ClusterBundle.ts
@@ -0,0 +1,168 @@
+import edgeVS from './ClusterBundle.vs.glsl';
+import edgeFS from './ClusterBundle.fs.glsl';
+import dataVS from './ClusterBundle.data.vs.glsl';
+
+import {App, DrawCall, PicoGL, Program, VertexArray, VertexBuffer} from 'picogl';
+import {GraferInputColor} from '../../../renderer/ColorRegistry';
+import {DataMappings, DataShader, kDataMappingFlatten} from '../../../data/DataTools';
+import {
+    GLDataTypes,
+    RenderableShaders,
+    RenderMode,
+    RenderUniforms,
+    setDrawCallUniforms,
+} from '../../../renderer/Renderable';
+import {Edges} from '../Edges';
+import {GraphPoints} from '../../../data/GraphPoints';
+import {PickingManager} from '../../../UX/picking/PickingManager';
+
+export interface ClusterBundleEdgeData {
+    id?: number | string;
+    source: number;
+    target: number;
+    sourceCluster: number;
+    targetCluster: number;
+    sourceColor?: GraferInputColor,
+    targetColor?: GraferInputColor,
+}
+
+export const kClusterBundleEdgeMappings: DataMappings<ClusterBundleEdgeData & { index: number[] }> = {
+    id: (entry: ClusterBundleEdgeData, i) => 'id' in entry ? entry.id : i,
+    source: (entry: ClusterBundleEdgeData) => entry.source,
+    target: (entry: ClusterBundleEdgeData) => entry.target,
+    sourceCluster: (entry: ClusterBundleEdgeData) => entry.sourceCluster,
+    targetCluster: (entry: ClusterBundleEdgeData) => entry.targetCluster,
+    sourceColor: (entry: ClusterBundleEdgeData) => 'sourceColor' in entry ? entry.sourceColor : 0, // first registered color
+    targetColor: (entry: ClusterBundleEdgeData) => 'targetColor' in entry ? entry.targetColor : 0, // first registered color
+    index: () => [0, 1, 2],
+};
+
+export const kClusterBundleEdgeDataTypes: GLDataTypes<ClusterBundleEdgeData & { index: number[] }> = {
+    source: PicoGL.UNSIGNED_INT,
+    target: PicoGL.UNSIGNED_INT,
+    sourceCluster: PicoGL.UNSIGNED_INT,
+    targetCluster: PicoGL.UNSIGNED_INT,
+    sourceColor: PicoGL.UNSIGNED_INT,
+    targetColor: PicoGL.UNSIGNED_INT,
+    index: PicoGL.UNSIGNED_INT,
+};
+
+export const kGLClusterBundleEdgeTypes = {
+    source: [PicoGL.FLOAT, PicoGL.FLOAT, PicoGL.FLOAT],
+    target: [PicoGL.FLOAT, PicoGL.FLOAT, PicoGL.FLOAT],
+    control: [PicoGL.FLOAT, PicoGL.FLOAT, PicoGL.FLOAT],
+    sourceColor: PicoGL.UNSIGNED_INT,
+    targetColor: PicoGL.UNSIGNED_INT,
+    colorMix: [PicoGL.FLOAT, PicoGL.FLOAT],
+} as const;
+export type GLClusterBundleEdgeTypes = typeof kGLClusterBundleEdgeTypes;
+
+export class ClusterBundle extends Edges<ClusterBundleEdgeData, GLClusterBundleEdgeTypes> {
+    protected program: Program;
+    protected drawCall: DrawCall;
+
+    protected verticesVBO: VertexBuffer;
+    protected edgesVAO: VertexArray;
+
+    constructor(context: App,
+                points: GraphPoints,
+                data: unknown[],
+                mappings: Partial<DataMappings<ClusterBundleEdgeData>>,
+                pickingManager: PickingManager,
+                segments: number = 16
+    ) {
+        super(context, points, data, mappings, pickingManager);
+
+        const segmentVertices = [];
+        for (let i = 0; i <= segments; ++i) {
+            segmentVertices.push(i / segments, 0);
+        }
+
+        this.verticesVBO = context.createVertexBuffer(PicoGL.FLOAT, 2, new Float32Array(segmentVertices));
+        this.edgesVAO = context.createVertexArray().vertexAttributeBuffer(0, this.verticesVBO);
+        this.configureTargetVAO(this.edgesVAO);
+
+        const shaders = this.getDrawShaders();
+        this.program = context.createProgram(shaders.vs, shaders.fs);
+        this.drawCall = context.createDrawCall(this.program, this.edgesVAO).primitive(PicoGL.LINE_STRIP);
+
+        this.compute(context, {
+            uGraphPoints: this.dataTexture,
+        });
+
+        // printDataGL(context, this.targetVBO, data.length, kGLStraightEdgeTypes);
+    }
+
+    public destroy(): void {
+        // TODO: Implement destroy method
+    }
+
+    public render(context:App, mode: RenderMode, uniforms: RenderUniforms): void {
+        setDrawCallUniforms(this.drawCall, uniforms);
+        setDrawCallUniforms(this.drawCall, this.localUniforms);
+
+        context.enable(PicoGL.BLEND);
+
+        context.depthRange(this.nearDepth, this.farDepth);
+        context.depthMask(false);
+
+        switch (mode) {
+            case RenderMode.PICKING:
+                // this.pickingDrawCall.draw();
+                break;
+
+            case RenderMode.HIGH_PASS_2:
+                break;
+
+            default:
+                this.drawCall.draw();
+                break;
+        }
+    }
+
+    protected getDrawShaders(): RenderableShaders {
+        return {
+            vs: edgeVS,
+            fs: edgeFS,
+        };
+    }
+
+    protected getPickingShaders(): RenderableShaders {
+        return {
+            vs: edgeVS,
+            fs: null, // pickingFS,
+        };
+    }
+
+    protected getGLSourceTypes(): GLDataTypes<ClusterBundleEdgeData> {
+        return kClusterBundleEdgeDataTypes;
+    }
+
+    protected getGLTargetTypes(): GLDataTypes<GLClusterBundleEdgeTypes> {
+        return kGLClusterBundleEdgeTypes;
+    }
+
+    protected getDataShader(): DataShader {
+        return {
+            vs: dataVS,
+            varyings: [ 'vSource', 'vTarget', 'vControl', 'vSourceColor', 'vTargetColor', 'vColorMix' ],
+        };
+    }
+
+    protected computeMappings(mappings: Partial<DataMappings<ClusterBundleEdgeData>>): DataMappings<ClusterBundleEdgeData> {
+        const edgesMappings = Object.assign({}, kClusterBundleEdgeMappings, super.computeMappings(mappings));
+
+        // patches the mappings to get the points index from their IDs
+        const sourceClusterMapping = edgesMappings.sourceCluster;
+        edgesMappings.sourceCluster = (entry, i): number => {
+            return this.points.getPointIndex(sourceClusterMapping(entry, i));
+        };
+
+        const targetClusterMapping = edgesMappings.targetCluster;
+        edgesMappings.targetCluster = (entry, i): number => {
+            return this.points.getPointIndex(targetClusterMapping(entry, i));
+        };
+
+        return edgesMappings as DataMappings<ClusterBundleEdgeData>;
+    }
+}
