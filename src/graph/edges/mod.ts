d330948a016c2c8e5355eacd09166815a053e98a
Dario Segura <dsegura@uncharted.software>
2020-12-07 15:17:50 -0500

    [GRAPH] Exposes ClusterBundle edge type

--- src/graph/edges/mod.ts
+++ src/graph/edges/mod.ts
@@ -2,17 +2,20 @@ export * from './straight/Straight';
 export * from './dashed/Dashed';
 export * from './gravity/Gravity';
 export * from './path/CurvedPath';
+export * from './bundle/ClusterBundle';
 
 import {Straight} from './straight/Straight';
 import {Dashed} from './dashed/Dashed';
 import {Gravity} from './gravity/Gravity';
 import {CurvedPath} from './path/CurvedPath';
+import {ClusterBundle} from './bundle/ClusterBundle';
 
 export const types = {
     Straight,
     Dashed,
     Gravity,
     CurvedPath,
+    ClusterBundle,
 };
 
 export* from './Edges';
