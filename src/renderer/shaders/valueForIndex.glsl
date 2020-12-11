47699143d417270b8a7027913cb4ea5e48477e11
Dario Segura <dsegura@uncharted.software>
2020-12-11 15:50:15 -0500

    [RENDERER] Added unsigned version to `valueForIndex` shader

--- src/renderer/shaders/valueForIndex.glsl
+++ src/renderer/shaders/valueForIndex.glsl
@@ -5,4 +5,11 @@ vec4 valueForIndex(sampler2D tex, int index) {
     return texelFetch(tex, ivec2(col, row), 0);
 }
 
+uvec4 uvalueForIndex(usampler2D tex, int index) {
+    int texWidth = textureSize(tex, 0).x;
+    int col = index % texWidth;
+    int row = index / texWidth;
+    return texelFetch(tex, ivec2(col, row), 0);
+}
+
 #pragma glslify: export(valueForIndex)
