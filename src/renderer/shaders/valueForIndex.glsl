8bbea24589b5df8ab3ad8aa35a85a89cd06ac5ce
Dario Segura <dsegura@uncharted.software>
2021-02-23 14:46:09 -0500

    [RENDERER] Added shader utility function

--- src/renderer/shaders/valueForIndex.glsl
+++ src/renderer/shaders/valueForIndex.glsl
@@ -12,4 +12,11 @@ uvec4 uvalueForIndex(usampler2D tex, int index) {
     return texelFetch(tex, ivec2(col, row), 0);
 }
 
+uint uivalueForIndex(usampler2D tex, int index) {
+    int texWidth = textureSize(tex, 0).x;
+    int col = index % texWidth;
+    int row = index / texWidth;
+    return texelFetch(tex, ivec2(col, row), 0)[0];
+}
+
 #pragma glslify: export(valueForIndex)
