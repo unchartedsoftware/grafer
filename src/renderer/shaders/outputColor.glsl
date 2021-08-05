7a62768c75090f8c9f3c5946373b13968b79d0b6
Dario Segura <dsegura@uncharted.software>
2021-08-05 13:58:56 -0400

    [SHADERS] Brightness is computed using mix

--- src/renderer/shaders/outputColor.glsl
+++ src/renderer/shaders/outputColor.glsl
@@ -8,7 +8,8 @@ uniform float uAlpha;
 
 vec4 outputColor(vec4 color) {
     // desaturate => fade => alpha
-    vec3 ret = vec3(desaturateColor(color.rgb + uBrightness, uDesaturate));
+    vec3 ret = mix(color.rgb, vec3(uBrightness + 1.0 / 2.0), abs(uBrightness));
+    ret = vec3(desaturateColor(ret, uDesaturate));
     ret = mix(ret, uClearColor.rgb, uFade);
     return vec4(ret, color.a * uAlpha);
 }
