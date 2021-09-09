adddcbe8584047c33a7529988d1153eaffe651dc
Dario Segura <dsegura@uncharted.software>
2021-09-09 15:56:31 -0400

    [UX] Drag interaction takes pixel ratio into consideration

--- src/UX/mouse/drag/DragTranslate.ts
+++ src/UX/mouse/drag/DragTranslate.ts
@@ -5,7 +5,7 @@ import {vec2} from 'gl-matrix';
 export class DragTranslate extends DragModule {
     protected handleMouse(event: symbol, state: MouseState, delta: vec2): void {
         if (state.buttons[this.button]) {
-            this.viewport.graph.translate([delta[0], -delta[1], 0]);
+            this.viewport.graph.translate([delta[0] * state.pixelRatio, -delta[1] * state.pixelRatio, 0]);
 
             this.viewport.render();
         }
