13b442c13d4f9ef1435c093c0f8a96f6dca9b133
Manfred Cheung <mj3cheun@uwaterloo.ca>
2023-09-22 12:58:40 -0400

    fix: pr comments

--- src/data/shaders/GraphPoints.fs.glsl
+++ src/data/shaders/GraphPoints.fs.glsl
@@ -14,7 +14,7 @@ uniform uint uRadiusHierarchyType;
 uniform uint uMaxHierarchyDepth;
 
 #pragma glslify: import(../../renderer/shaders/valueForIndex.glsl)
-#pragma glslify: import(./classMode.glsl)
+#pragma glslify: import(./hierarchyType.glsl)
 
 void main() {
     vec2 texSize = vec2(textureSize(uPointTexture, 0).xy);
@@ -22,15 +22,15 @@ void main() {
     fragColor = texelFetch(uPointTexture, coords, 0);
 
     uint i = 0u;
-    int classIndex = texelFetch(uParentTexture, coords, 0).x;
-    while(classIndex != -1 && i++ < uMaxHierarchyDepth) {
-        vec4 point = valueForIndex(uPointTexture, classIndex);
+    int parentIndex = texelFetch(uParentTexture, coords, 0).x;
+    while(parentIndex != -1 && i++ < uMaxHierarchyDepth) {
+        vec4 point = valueForIndex(uPointTexture, parentIndex);
         if(uPositionHierarchyType == MODE_ADD) {
             fragColor.xyz += point.xyz;
         }
         if(uRadiusHierarchyType == MODE_ADD) {
             fragColor.w += point.w;
         }
-        classIndex = ivalueForIndex(uParentTexture, classIndex).x;
+        parentIndex = ivalueForIndex(uParentTexture, parentIndex).x;
     }
 }
